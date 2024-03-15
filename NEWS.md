4dbbca49d5cd6403a76c19780c6a3291d70bf332
timothy-barry <tbarry2@andrew.cmu.edu>
2024-03-15 16:43:52 -0400

    add files to repo

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# sceptredata 0.9.0
+
+`sceptredata` 0.9.0 is the first release of the `sceptredata` package.
--- R/data.R
+++ R/data.R
@@ -30,10 +30,3 @@
 #'
 #' @usage data(lowmoi_example_data)
 "lowmoi_example_data"
-
-#' Gene position data frame
-#'
-#' `gene_position_data_frame_grch38` maps each gene to the chromosome on which it is located and to the position of its transcription start site on that chromosome. The data frame was constructed from the GRCh38 reference genome that has shipped with CellRanger since 2020.
-#'
-#' @usage data(gene_position_data_frame_grch38)
-"gene_position_data_frame_grch38"
